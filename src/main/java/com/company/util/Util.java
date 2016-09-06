012bc7e7e18cbad1be7cd35289f2e276467cb336
khomyakeugene <khomyak_eugene@yahoo.com>
2016-09-06 13:22:25 +0300

    Another refactoring

--- src/main/java/com/company/util/Util.java
+++ src/main/java/com/company/util/Util.java
@@ -280,6 +280,22 @@ public class Util {
         return new Timestamp(timestamp.getTime() + dayToMiliseconds(days));
     }
 
+    public static Date getDateOnly(Date date) {
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        return calendar.getTime();
+    }
+
+    public static Date getDateOnly(Timestamp timestamp) {
+        return getDateOnly(new Date(timestamp.getTime()));
+    }
+
     public static String getApplicationName() {
         String result;
 
